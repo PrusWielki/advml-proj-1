5137004e320daad2149088da93a5e450ceece863
Patryk <pruswielki@gmail.com>
2024-03-29 18:29:41 +0100

    progress

--- logisticRegression.py
+++ logisticRegression.py
@@ -2,20 +2,42 @@ import numpy as np
 
 
 class LogisticRegression:
-    def __init__(self, noOfIterations):
+    def __init__(self, noOfIterations, learningRate):
         self.w= []
         self.noOfIterations = noOfIterations
+        self.learningRate = learningRate
         pass
-    def fit(self,X):
+    def fit(self,X,y):
         self.w = np.random.randint(0,1,size=X.shape[1])
+        self.bias=0
         costs=[]
         for i in range(self.noOfIterations):
             
-            a = np.dot(X,self.w)
-            # link function
+            a = np.dot(X,self.w) +self.bias
+
             yHat=1/(1+np.exp(-a))
 
-            # cost function
-            cost = (-1/X.shape[0])
+            # cost function (cross entropy loss)
+            costs.append((-1/X.shape[0])(np.dot(y,yHat)+np.dot((1-y),np.log(1-yHat))))
+            
+            # update weights
+            weightChange = (1/X.shape[0])*np.dot(X.T,(yHat-y))
+            biasChange = (1/X.shape[0]) * np.sum(yHat-y)
+
+            self.w -= self.learningRate*weightChange
+            self.bias-=self.learningRate*biasChange
+
+    def predict(self,X):
+        predicted  = 1/(1+np.exp(-(np.dot(X,self.w) +self.bias)))
+        predictedClasses = []
+        for x in predicted:
+            if x>0.5:
+                predictedClasses.append(1)
+            else:
+                predictedClasses.append(0)
+
+        return predictedClasses
+
+            
 
 
