f6ec873b1dcdb0ee26358802a4e93de85f377e5f
Patryk <pruswielki@gmail.com>
2024-03-29 18:30:29 +0100

    progress

--- logisticRegression.py
+++ logisticRegression.py
@@ -6,11 +6,12 @@ class LogisticRegression:
         self.w= []
         self.noOfIterations = noOfIterations
         self.learningRate = learningRate
+        self.costs=[]
         pass
     def fit(self,X,y):
         self.w = np.random.randint(0,1,size=X.shape[1])
         self.bias=0
-        costs=[]
+        self.costs=[]
         for i in range(self.noOfIterations):
             
             a = np.dot(X,self.w) +self.bias
@@ -18,7 +19,7 @@ class LogisticRegression:
             yHat=1/(1+np.exp(-a))
 
             # cost function (cross entropy loss)
-            costs.append((-1/X.shape[0])(np.dot(y,yHat)+np.dot((1-y),np.log(1-yHat))))
+            self.costs.append((-1/X.shape[0])(np.dot(y,yHat)+np.dot((1-y),np.log(1-yHat))))
             
             # update weights
             weightChange = (1/X.shape[0])*np.dot(X.T,(yHat-y))
