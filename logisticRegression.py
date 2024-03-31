1b536b098634a1a0a7e760caffc9a9f8468b9c66
PrusWielki <prusakpat@gmail.com>
2024-03-31 20:55:35 +0200

    add iwls

--- logisticRegression.py
+++ logisticRegression.py
@@ -19,8 +19,11 @@ class LogisticRegression:
                 self.fitSgd(X,y)
             case Optimizer.Adam:
                 self.fitAdam(X,y)
+            case Optimizer.IWLS:
+                self.fitIwls(X,y)
             
     def predict(self,X):
+        print(self.w)
         predicted  = 1/(1+np.exp(-(np.dot(X,self.w).astype(float) +self.bias)).astype(float))
         predictedClasses = []
         for x in predicted:
@@ -101,4 +104,18 @@ class LogisticRegression:
             self.bias = self.bias-self.learningRate*mhatBias/(np.sqrt(vhatBias)+epsilon)
 
     def fitIwls(self,X,y):
-        pass           
+        self.w = np.zeros(X.shape[1])
+        self.costs=[]
+        self.bias=0
+        for i in range(1,self.noOfIterations+1):
+            a = np.dot(X,self.w) 
+            a=a.astype(float)
+            yHat=1.0/(1.0+np.exp(-a))
+            self.costs.append((-1.0/X.shape[0])*(np.dot(y,np.log(yHat,out=np.zeros_like(yHat), where=(yHat!=0)))+np.dot((1.0-y),np.log(1.0-yHat,out=np.zeros_like(yHat), where=(yHat!=1)))))
+            # using formula from lecture slides (X^TWX )^−1X^TWz
+            # z = X βold + W^−1(y − p), Beta being weights
+            pp=yHat*(1-yHat)
+            W = np.diag(pp)
+            z = a+ np.dot(np.linalg.inv(W),y-yHat)
+            # (np.divide((y-yHat),pp,out=np.zeros_like(pp),where=pp!=0))
+            self.w=np.dot(np.dot(np.dot(np.linalg.inv(np.dot(np.dot(X.T,W),X)),X.T),W),z)
