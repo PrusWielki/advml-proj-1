a86e71c5bf6a72bfde7297c0afec9e9593ed9b61
Patryk <78270281+PrusWielki@users.noreply.github.com>
2024-03-31 20:56:20 +0200

    IWLS (#5)
    * adam fixes
    
    * add iwls

--- logisticRegression.py
+++ logisticRegression.py
@@ -19,8 +19,11 @@ class LogisticRegression:
                 self.fitSgd(X,y)
             case Optimizer.Adam:
                 self.fitAdam(X,y)
+            case Optimizer.IWLS:
+                self.fitIwls(X,y)
             
     def predict(self,X):
+        print(self.w)
         predicted  = 1/(1+np.exp(-(np.dot(X,self.w).astype(float) +self.bias)).astype(float))
         predictedClasses = []
         for x in predicted:
@@ -69,39 +72,50 @@ class LogisticRegression:
         moment2Weights=np.zeros(X.shape[1])
         moment1Bias=0
         moment2Bias=0
-        mhat=0
-        vhat=0
         
-        for i in range(self.noOfIterations):
+        for i in range(1,self.noOfIterations+1):
             
             a = np.dot(X,self.w) +self.bias
             a=a.astype(float)
-            yHat=1/(1+np.exp(-a))
+            yHat=1.0/(1.0+np.exp(-a))
 
             # cost function (cross entropy loss)
-            self.costs.append((-1/X.shape[0])*(np.dot(y,np.log(yHat))+np.dot((1-y),np.log(1-yHat))))
+            self.costs.append((-1.0/X.shape[0])*(np.dot(y,np.log(yHat))+np.dot((1.0-y),np.log(1.0-yHat))))
             
             # Calculate gradient of cross entropy with respect to weights and bias
             # From what I understand the gradient is simply y predicted - y but in this article https://medium.com/analytics-vidhya/implementing-logistic-regression-with-sgd-from-scratch-5e46c1c54c35
             # they claim that gradient with respect to weights should be x(y-y_pred) and with respect to bias: y- ypred
             gradientWithRespectToWeights=np.dot(X.T,yHat-y)
-            gradientWithRespectToBias = yHat-y
+            gradientWithRespectToBias = np.sum(yHat-y)
             moment1Weights=beta1*moment1Weights+(1-beta1)*gradientWithRespectToWeights
             moment2Weights=beta2*moment2Weights+(1-beta2)*gradientWithRespectToWeights*gradientWithRespectToWeights
-
-            mhatWeights = moment1Weights/(1-beta1**i)
-            vhatWeights = moment2Weights/(1-beta2**i)
+            mhatWeights = moment1Weights/(1.0-beta1**i)
+            vhatWeights = moment2Weights/(1.0-beta2**i)
 
             self.w = self.w-self.learningRate*mhatWeights/(np.sqrt(vhatWeights)+epsilon)
 
-            moment1Bias=beta1*moment1Bias+(1-beta1)*gradientWithRespectToBias
+            moment1Bias=beta1*moment1Bias+(1.0-beta1)*gradientWithRespectToBias
             # We could iterate over all xs and do it one by one, but I think it is equivalent to summing the moment2 for all xs and updating the bias at once
-            moment2Bias=np.dot(beta2*moment2Bias+(1-beta2),gradientWithRespectToBias*gradientWithRespectToBias)
+            moment2Bias=np.dot(beta2*moment2Bias+(1.0-beta2),gradientWithRespectToBias*gradientWithRespectToBias)
 
-            mhatBias = moment1Bias/(1-beta1**i)
-            vhatBias = moment2Bias/(1-beta2**i)
+            mhatBias = moment1Bias/(1.0-beta1**i)
+            vhatBias = moment2Bias/(1.0-beta2**i)
 
             self.bias = self.bias-self.learningRate*mhatBias/(np.sqrt(vhatBias)+epsilon)
 
     def fitIwls(self,X,y):
-        pass           
+        self.w = np.zeros(X.shape[1])
+        self.costs=[]
+        self.bias=0
+        for i in range(1,self.noOfIterations+1):
+            a = np.dot(X,self.w) 
+            a=a.astype(float)
+            yHat=1.0/(1.0+np.exp(-a))
+            self.costs.append((-1.0/X.shape[0])*(np.dot(y,np.log(yHat,out=np.zeros_like(yHat), where=(yHat!=0)))+np.dot((1.0-y),np.log(1.0-yHat,out=np.zeros_like(yHat), where=(yHat!=1)))))
+            # using formula from lecture slides (X^TWX )^−1X^TWz
+            # z = X βold + W^−1(y − p), Beta being weights
+            pp=yHat*(1-yHat)
+            W = np.diag(pp)
+            z = a+ np.dot(np.linalg.inv(W),y-yHat)
+            # (np.divide((y-yHat),pp,out=np.zeros_like(pp),where=pp!=0))
+            self.w=np.dot(np.dot(np.dot(np.linalg.inv(np.dot(np.dot(X.T,W),X)),X.T),W),z)
